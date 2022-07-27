b03d373d4cb6ca91f63c29a48ad35faa180e83cb
surtarso <tarso.galvao@gmail.com>
2022-07-27 20:37:40 -0300

    comments

--- learningNode/5-REST/src/routes/users.route.ts
+++ learningNode/5-REST/src/routes/users.route.ts
@@ -3,6 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
+//-----------CREATE
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+//-----------READ
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'tarso' }];
     res.status(StatusCodes.OK).send({ users });
@@ -13,18 +20,15 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
     res.sendStatus(StatusCodes.OK).send({ uuid });
 });
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
-    const newUser = req.body;
-    res.status(StatusCodes.CREATED).send(newUser);
-});
-
+//----------UPDATE
 usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
     modifiedUser.uuid = uuid;
-    res.status(StatusCodes.OK).send({ modifiedUser });
+    res.status(StatusCodes.CREATED).send({ modifiedUser });
 });
 
+//----------DELETE
 usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     res.status(StatusCodes.OK);
 });
