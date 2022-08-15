2c88face5dd12e7af77aff1a7151943f0f2c7183
surtarso <tarso.galvao@gmail.com>
2022-08-14 22:03:52 -0300

    auto updated by script

--- learningNode/5-REST/src/db.ts
+++ learningNode/5-REST/src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 //dbuser:password@database.server.com:port/mydtb
-const connectionString = '';
+const connectionString = 'postgres://ynsxfwbc:Gf7NnJuh6Lmum9okAmf8NLXwk8nVTylI@kesavan.db.elephantsql.com/ynsxfwbc';
 
 const db = new Pool({ connectionString });
 
